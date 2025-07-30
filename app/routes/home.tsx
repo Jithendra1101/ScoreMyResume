cfc59fa67403cbbea0549de1206796936ba76082
Jithendra Varma <gjithendravarma@gmail.com>
2025-07-30 20:53:05 +0530

    Hero Section and Navbar BoilerPlate is made

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,22 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import Navbar from "~/components/navbar";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "ScoreMyResume" },
+    { name: "description", content: "ScoreMyResume is a React-based web app that analyzes your resume against any job description and provides an ATS compatibility score. Instantly see how well your resume aligns and get insights to improve your chances of getting noticed." },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return (
+  <main className='bg-[url("/images/bg-main.svg")] bg-cover'>
+    <Navbar/>
+  <section className="main-section"> 
+  <div className="page-heading">
+    <h1>Apply smart. Track better. Win faster.</h1>
+    <h2>Get smart insights on your resume, powered by AI</h2>
+  </div>
+  </section>
+  </main>);
 }
